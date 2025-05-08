ca8396fb04ba57c0c6eb4e5be1cd96f0b68c9005
Toyehamzat <anibiseun@gmail.com>
2025-05-08 23:22:12 +0100

    feat: enable POST function in liveblocks-auth route for room data handling

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -3,17 +3,17 @@
 // import { auth, currentUser } from "@clerk/nextjs/server";
 
 // const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-// export async function POST(req: Request) {
-//   const { sessionClaims } = await auth();
+export async function POST(req: Request) {
+  //   const { sessionClaims } = await auth();
 
-//   if (!sessionClaims) {
-//     return new Response("Unauthorized", { status: 401 });
-//   }
+  //   if (!sessionClaims) {
+  //     return new Response("Unauthorized", { status: 401 });
+  //   }
 
-//   const user = await currentUser();
-//   if (!user) {
-//     return new Response("Unauthorized", { status: 401 });
-//   }
-//   const {room} =await req.json()
-
-// }
+  //   const user = await currentUser();
+  //   if (!user) {
+  //     return new Response("Unauthorized", { status: 401 });
+  //   }
+  const { room } = await req.json();
+  console.log("room", room);
+}
