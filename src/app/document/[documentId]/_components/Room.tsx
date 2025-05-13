6ab2e9170a7b27ae68c47012ccfc0c156861885e
Toyehamzat <anibiseun@gmail.com>
2025-05-13 12:26:12 +0100

    feat: implement user fetching logic and enhance LiveblocksProvider configuration

--- src/app/document/[documentId]/_components/Room.tsx
+++ src/app/document/[documentId]/_components/Room.tsx
@@ -1,19 +1,70 @@
 "use client";
 
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useMemo, useState } from "react";
 import {
   LiveblocksProvider,
   RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import { useParams } from "next/navigation";
+import { FullScreenLoader } from "@/components/fullScreenLoader";
+import { GetUser } from "../action";
+import { toast } from "sonner";
 
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
 export function Room({ children }: { children: ReactNode }) {
   const params = useParams();
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  const fetchUsers = useMemo(
+    () => async () => {
+      try {
+        const list = await GetUser();
+        setUsers(list);
+      } catch (error) {
+        toast.error("Error fetching users");
+        console.error("Error fetching users:", error);
+      }
+    },
+    []
+  );
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
   return (
-    <LiveblocksProvider authEndpoint="/api/liveblocks-auth" throttle={16}>
+    <LiveblocksProvider
+      authEndpoint="/api/liveblocks-auth"
+      throttle={16}
+      resolveUsers={({ userIds }) => {
+        return (
+          userIds.map((userId) => users.find((user) => user.id === userId)) ??
+          undefined
+        );
+      }}
+      resolveRoomsInfo={() => []}
+      resolveMentionSuggestions={({ text }) => {
+        let filteredUsers = users;
+
+        if (text) {
+          filteredUsers = users.filter((user) =>
+            user.name.toLowerCase().includes(text.toLowerCase())
+          );
+        }
+
+        return filteredUsers.map((user) => user.id);
+      }}
+    >
       <RoomProvider id={params.documentId as string}>
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
+        <ClientSideSuspense
+          fallback={<FullScreenLoader label="Room loading..." />}
+        >
           {children}
         </ClientSideSuspense>
       </RoomProvider>
