7d95a91c0b4621c02662c668ca5abea6957c1150
Toyehamzat <anibiseun@gmail.com>
2025-05-13 12:25:55 +0100

    fix: restructure page component layout for improved readability

--- src/app/document/[documentId]/page.tsx
+++ src/app/document/[documentId]/page.tsx
@@ -15,16 +15,16 @@ export async function generateMetadata({ params }: DocumentPageProps) {
 }
 export default async function page() {
   return (
-    <div className="min-h-screen bg-[#FAFBFD]">
-      <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 bg-[#FAFBFD] z-10  print:hidden">
-        <Navbar />
-        <ToolBar />
-      </div>
-      <div className="pt-[114px] print:pt-0">
-        <Room>
+    <Room>
+      <div className="min-h-screen bg-[#FAFBFD]">
+        <div className="flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 bg-[#FAFBFD] z-10  print:hidden">
+          <Navbar />
+          <ToolBar />
+        </div>
+        <div className="pt-[114px] print:pt-0">
           <Editor />
-        </Room>
+        </div>
       </div>
-    </div>
+    </Room>
   );
 }
