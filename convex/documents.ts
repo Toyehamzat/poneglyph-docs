acdff87717363dcaa4ef056b36def7c39166fe0f
Toyehamzat <anibiseun@gmail.com>
2025-05-14 11:53:18 +0100

    feat: add getByIds query to fetch multiple documents with handling for missing entries

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,7 +1,24 @@
-import { query } from "./_generated/server";
-import { mutation } from "./_generated/server";
+import { query, mutation } from "./_generated/server";
 import { ConvexError, v } from "convex/values";
 import { paginationOptsValidator } from "convex/server";
+
+export const getByIds = query({
+  args: { ids: v.array(v.id("documents")) },
+  handler: async (ctx, { ids }) => {
+    const documents = [];
+
+    for (const id of ids) {
+      const document = await ctx.db.get(id);
+
+      if (document) {
+        documents.push({ id: document._id, name: document.title });
+      } else {
+        documents.push({ id, name: "[Removed document]" });
+      }
+    }
+    return documents;
+  },
+});
 
 export const create = mutation({
   args: {
@@ -116,7 +133,7 @@ export const updateById = mutation({
     }
     return await ctx.db.patch(args.id, { title: args.title });
   },
-});  
+});
 
 export const getById = query({
   args: { id: v.id("documents") },
