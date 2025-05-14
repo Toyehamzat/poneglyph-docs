0bfedb8796201222d0ac385097c3d0e8dd7bca46
Toyehamzat <anibiseun@gmail.com>
2025-05-14 17:23:11 +0100

    feat: add Loading component for document loading state

---
+++ src/app/document/[documentId]/loading.tsx
@@ -0,0 +1,6 @@
+import { FullScreenLoader } from "@/components/fullScreenLoader";
+import React from "react";
+
+export default function Loading() {
+  return <FullScreenLoader label="Document Loading" />;
+}
